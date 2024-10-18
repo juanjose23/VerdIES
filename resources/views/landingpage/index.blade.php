7309bb35a5ff11aaddc13aab8f14463cbf02d55a
juanjose23 <jrios9836@gmail.com>
2024-10-18 10:10:00 -0600

    🔇feature:spotify

--- resources/views/landingpage/index.blade.php
+++ resources/views/landingpage/index.blade.php
@@ -138,6 +138,9 @@
                                 </div>
                             </div>
                         </article>
+                        <article  class="col-xl-6 col-lg-6 col-md-6">
+                            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/1ibTlqUyamBSpAmo6ZTNP1?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                        </article>
                         <article class="col-xl-6 col-lg-6 col-md-6">
                             <div class="feature-item mb-70">
                                 <div class="feature-icon">
