bd63f649131a6443c252d87a7f2dfe096e7f3424
juanjose23 <jrios9836@gmail.com>
2024-10-17 22:33:31 -0600

    Bug: Migracion de categoria corregido

--- database/migrations/2024_06_09_034338_create_categorias_table.php
+++ database/migrations/2024_06_09_034338_create_categorias_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categorias', function (Blueprint $table) {
+        Schema::create('categoria', function (Blueprint $table) {
             $table->id();
             $table->string('nombre')->unique();
             $table->text('descripcion')->nullable();
